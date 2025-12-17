9dd1914adc698489021ddc2154f15489c209e97c
Juan Manuel <juan.manuel.amaya.suarez@gmail.com>
2025-12-17 06:25:42 -0500

    Actualizacion 17.12.2025 v1e Vercel Update

--- app/map-google/page.tsx
+++ app/map-google/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import GoogleMap from './GoogleMap';
+//import GoogleMap from './GoogleMap';
 
-export default function MapGooglePage() {
-  return (
-    <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
-      <GoogleMap />
-    </div>
-  );
-}
+//export default function MapGooglePage() {
+//  return (
+//    <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
+//      <GoogleMap />
+//    </div>
+//  );
+//}
